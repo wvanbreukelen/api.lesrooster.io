5198acd07fc066f4f11856f9c0e821f5d63c1639
Wiebe van Breukelen <wvanbreukelen@users.noreply.github.com>
2016-01-25 20:41:24 +0100

    Fix

--- www/lib/Zermelo/ZermeloHelper.php
+++ www/lib/Zermelo/ZermeloHelper.php
@@ -323,23 +323,23 @@ class ZermeloHelper
 		$timestamps = array();
 		foreach ($grid as $key => $node)
 		{
-		if (in_array($node['start'], $timestamps))
-		   {
-		    if (self::ALLOW_DOUBLE_HOURS == false)
-		     {
-		      unset($grid[$key]);
-		     } else {
-		      $timestamps[$key] = $node['start'];
-		     }
-		     
-		   } else {
-		    if ($node['cancelled'] == true)
-		     {
-		      unset($grid[$key]);
-		     } else {
-		     $timestamps[$key] = $node['start'];
-		    }
-		   }
+			if (in_array($node['start'], $timestamps))
+			   {
+			    if (self::ALLOW_DOUBLE_HOURS == false)
+			     {
+			      unset($grid[$key]);
+			     } else {
+			      $timestamps[$key] = $node['start'];
+			     }
+			     
+			     if ($node['cancelled'] == true)
+			     {
+			      unset($grid[$key]);
+			     } else {
+			     $timestamps[$key] = $node['start'];
+			     }
+			   }
+			
 		}
 		array_multisort($timestamps, SORT_ASC, $grid);
 		return $grid;
